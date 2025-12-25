3b353997b96dee2bc261fae0175100c0d6590111
Xuyen_Blockchain_Dev <35366890+xuyen88@users.noreply.github.com>
2025-12-25 16:41:44 +0700

    Need these for test with node.js newese version

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+import "@nomicfoundation/hardhat-toolbox";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+export default {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
